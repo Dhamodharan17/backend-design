cdf60c53c976fbf56481d564122b98d61bb7ca89
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-07-09 15:39:15 +0530

    ocp

---
+++ solid/OCP.java
@@ -0,0 +1,57 @@
+public class OCP {
+
+  public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        
+        Operation addOperation = new AddOperation();
+        
+        int result = calculator.calculateNumber(10, 5, addOperation);
+        
+        System.out.println("The result of addition is: " + result);
+    }
+}
+
+
+// bad calculator design - we need to touch exisiting code if we want another operation
+class BadCalculator{
+    public int calculateNumber(int number1,int number2, String type){
+        int result=0;
+        switch (type) {
+            case "sun":
+                result = number1+number2;
+                break;
+            case "sub":
+                result = number1-number2;
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+}
+
+//good design - no need to touch exisiting code, create another operation class 
+
+interface Operation{
+    public int perfrom(int number1,int number2);
+}
+
+class AddOperation implements Operation{
+    public int perfrom(int number1,int number2){
+        return number1+number2;
+    }
+}
+
+class SubOperation implements Operation{
+    public int perfrom(int number1,int number2){
+        return number1-number2;
+    }
+}
+
+class Calculator{
+    public int calculateNumber(int number1,int number2,Operation operation){
+        return operation.perfrom(number1, number2);
+    }
+}
+
+
