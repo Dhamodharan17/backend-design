0dc9b146cedc2046da7e7d8311a6fa56d6f825b4
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-07-09 19:12:20 +0530

    overview of dp

---
+++ design-patterns/Overview.java
@@ -0,0 +1,38 @@
+public Main{
+
+  main(){
+  /*
+ Creational
+1.Singleton - only one object of the class e.g db connection object
+2.Factory - there will be factory class which creates object based on the given object
+3.Abstact Factory - factory that will creates family of object (related objects)
+4..buillder - helps create object [ avoid multiple unused constrcutor(director->create objects with default values)
+5.prototype -when object creation is heavy (clonable) [registry - hashmap]
+
+Structural - helps to structure our application
+1.Adapter - converting one object into another
+2.Bridge - convert "IS a" to "Has a"
+3.Decorter - add behaviour of one object into special wrapper[ base class+different decortors]
+4.facade - whenever working with 3rd party library, we want only part of library 
+5.Proxy - before calling object add another layer with buisness logic
+6.composite - will handle the add,remove and print and it is like manager(hirercy of classes)
+
+Behavorial 
+1. Chain of responsibility - passing data from one logic to another
+2. command -don't directly execute request on buisness logic, execute command that will execure buiness logic
+3.Iterator - allows us to traverse all of the data strcuture
+4.mediator - b/w different object and behavior and reduces dependcies
+5.observer - when there is change all the subscribers will be altered
+6.flyweight -object creation process is heavy, it helps to utlize existing object with intrisctic properties
+7.interpretor - expresion matching
+8.memento - when we want to store state of a object and restore object
+9.state design pattern - instead of if else, based on contect, action will taken
+10.template - define set of instrcution that need to be folloes and make client to change some instruction
+11.startegy - instead of if else, we reach stategy will execure different algorthim
+
+
+  
+  */
+    
+  }
+}
