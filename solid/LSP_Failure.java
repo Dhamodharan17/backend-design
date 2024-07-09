64d8dcaeb7afc9b3dbdad33ff34f720da826e1b5
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-07-09 18:37:42 +0530

    lsp

--- solid/LSP_Failure.java
+++ solid/LSP_Failure.java
@@ -2,7 +2,11 @@ public class LSP_V1 {
 
     //LSP - I can use object child class instead on parent class
     public static void main(String[] args) {
+        /*
+        , if you depend on an abstract Vehicle class instead of specific implementations like Car 
+        or Bike, you can easily introduce new types of vehicles without modifying existing code.
         
+        */
         Vehicle bike = new Bike();
         Vehicle car = new Car();
         bike.type();
