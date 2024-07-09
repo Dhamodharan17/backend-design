8b8813507b31bd38c319d2adfff9682cae7e7df2
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-07-09 15:49:20 +0530

    srp updated

--- solid/SRP.java
+++ solid/SRP.java
@@ -1,16 +1,28 @@
+package com.backend.solid;
+
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 public class SRP {
     // target class - Account, Account Operations - check it follows SRP
     // SRP - class should only contains methods and attributes realted to itself only.
+    public static void main(String[] args) {
+
+        Account account = new Account(10023,"msd",new BigDecimal(1));
+        System.out.println(account.getAccountNumber());
+    }
 }
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 class Account{
     private int accountNumber;
     private String firstName;
@@ -31,7 +43,7 @@ class AccountOperations{
 
     // responsiblity of Account - add, get, delete account
     // deposit is not responisbility of Account
-    public void deposit(int amount,int accountNumber){
+    public void deposit(BigDecimal amount,int accountNumber){
         // deposit operation
         accountMap.get(accountNumber).getTotalAmonut().add(amount);
     }
@@ -44,13 +56,13 @@ class TransactionOperations{
         // get account details from account operation
         AccountOperations accountOperations = new AccountOperations();
         Account account = accountOperations.geAccount(accountNumber);
-        account.setTotalAmout(account.getTotalAmonut().add(amount));
+        account.setTotalAmonut(account.getTotalAmonut().add(amount));
     }
 
     public void withdraw(BigDecimal amount,int accountNumber){
         AccountOperations accountOperations = new AccountOperations();
         Account account = accountOperations.geAccount(accountNumber);
-        account.setTotalAmout(account.getTotalAmonut().subtract(amount));
+        account.setTotalAmonut(account.getTotalAmonut().subtract(amount));
     }
 
 
